4e6757a134e2c9166f7eaff96575b7b2d4ddc575
Paul Hallaste <peeterpaul.hallaste@gmail.com>
2022-11-11 09:21:45 +0900

    Add users staging model

---
+++ data-orchestration/dags/dbt/data_community/models/staging/stg_users.sql
@@ -0,0 +1,15 @@
+with source_data as (
+    select
+        username,
+        name as user_full_name,
+        id,
+        public_metrics,
+        pinned_tweet_id,
+        description as user_description,
+        created_at,
+        verified
+    from {{ source('public', 'raw_twitter_users')}}
+)
+
+select *
+from source_data
