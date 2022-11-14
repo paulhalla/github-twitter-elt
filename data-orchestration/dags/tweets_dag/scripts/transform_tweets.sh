f434f13ad67eb7c64ffbe327154b968576b42037
Kingmoh <rashidmincan@gmail.com>
2022-11-15 01:17:48 +1000

    fix model selectors

--- data-orchestration/dags/tweets_dag/scripts/transform_tweets.sh
+++ data-orchestration/dags/tweets_dag/scripts/transform_tweets.sh
@@ -3,5 +3,5 @@
 cp -R /opt/airflow/dags/dbt /tmp;
 cd /tmp/dbt/data_community;
 /usr/local/airflow/dbt_env/bin/dbt deps;
-/usr/local/airflow/dbt_env/bin/dbt build --project-dir /tmp/dbt/data_community/ --profiles-dir . --target dev;
+/usr/local/airflow/dbt_env/bin/dbt run --select +all_top_tweets --project-dir /tmp/dbt/data_community/ --profiles-dir . --target dev;
 # cat /tmp/dbt/data_community/logs/dbt.log;
