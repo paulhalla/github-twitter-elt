80728b8dbdc3117339fb3fd930cff219685cdf15
Kingmoh <rashidmincan@gmail.com>
2022-11-15 19:53:39 +1000

    Created one big twitter table

---
+++ data-orchestration/dags/dbt/data_community/models/serving/twitter/one_big_twitter_table.sql
@@ -0,0 +1,43 @@
+with tweets as (
+    select * 
+    from {{ ref('stg_tweets') }}
+),
+
+users as (
+    select * 
+    from {{ ref('stg_users') }}
+),
+
+joined as (
+    select 
+        user_id,
+        users.user_full_name,
+        user_description,
+        users.created_at as user_joined_at,
+        tweets.username,
+        tweet_created_at,
+        edit_history_tweet_ids,
+        id,
+        tweet,
+        number_of_likes,
+        number_of_followers,
+        number_of_quotes,
+        number_of_replies,
+        number_of_retweets,
+        number_of_following,
+        number_of_tweets,
+        number_of_twitter_lists,
+        pinned_tweet_id,
+        tweets.last_load_time
+    from tweets 
+    inner join users 
+        on tweets.username = users.username
+),
+
+deduped as (
+    select *
+    from joined 
+    qualify row_number() over (partition by id order by id) = 1
+)
+
+select * from deduped
--- data-orchestration/dags/dbt/data_community/models/staging/public/stg_tweets.sql
+++ data-orchestration/dags/dbt/data_community/models/staging/public/stg_tweets.sql
@@ -1,32 +1,6 @@
-
-with load_times as (
+with source as (
     select * 
-    from {{ ref('base_public_raw_tweets_snapshot') }}
-),
-
-joined as (
-    select 
-        raw_tweets.username,
-        tweet_created_at,
-        edit_history_tweet_ids,
-        id,
-        tweet,
-        number_of_likes,
-        number_of_quotes,
-        number_of_replies,
-        number_of_retweets,
-        coalesce(load_times.last_load_time, current_timestamp())::timestamp as last_load_time
-    from {{ ref('base_public_raw_tweets') }} raw_tweets
-    left join load_times 
-        on raw_tweets.username = load_times.username
-),
-
-remove_dupes as (
-    select *,
-        row_number() over(partition by id order by tweet_created_at) row_num
-    from joined
+    from {{ source('public', 'stg_public__tweets') }}
 )
 
-select *
-from remove_dupes
-where row_num = 1
+select * from source
