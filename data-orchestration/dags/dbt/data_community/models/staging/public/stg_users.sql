c190b2e59eb41e2aaf34096c538c71496227e512
Paul Hallaste <peeterpaul.hallaste@gmail.com>
2022-11-18 16:48:07 +0900

    change serving_ prefix to ingest_ in all instances

--- data-orchestration/dags/dbt/data_community/models/staging/public/stg_users.sql
+++ data-orchestration/dags/dbt/data_community/models/staging/public/stg_users.sql
@@ -1,6 +1,6 @@
 with source as (
     select * 
-    from {{ source('public', 'serving_twitter_users') }}
+    from {{ source('public', 'ingest_twitter_users') }}
 )
 
 select * from source
--- data-orchestration/dags/dbt_docs_dag/tasks/push_docs.py
+++ data-orchestration/dags/dbt_docs_dag/tasks/push_docs.py
@@ -13,21 +13,21 @@ def push_docs():
     # s3 bucket config
     s3_client = boto3.client('s3')
     bucket_name = config.get('s3').get('bucket_name')
-    serving_folder = config.get('s3').get('serving_folder')
+    ingest_folder = config.get('s3').get('ingest_folder')
 
     # set up logging
     logging.basicConfig(level=logging.INFO, format="[%(levelname)s][%(asctime)s]: %(message)s")
 
     # list files
-    serving_files = [file for file in config.get('s3').get('docs_files')]
+    ingest_files = [file for file in config.get('s3').get('docs_files')]
 
-    # serving files path 
-    serving_files_path = config.get('s3').get('docs_files_path')
+    # ingest files path 
+    ingest_files_path = config.get('s3').get('docs_files_path')
 
     # push the files 
-    for s3_file in serving_files:
+    for s3_file in ingest_files:
         try:
-            s3_client.upload_file(f'{serving_files_path}/{s3_file}', bucket_name, f'{s3_file}', ExtraArgs={'ContentType':'text/html'})
+            s3_client.upload_file(f'{ingest_files_path}/{s3_file}', bucket_name, f'{s3_file}', ExtraArgs={'ContentType':'text/html'})
             logging.info(f'Successfully pushed {s3_file} to s3://{bucket_name}/{s3_file}')
         except ClientError as e:
             logging.error(e)
