2aa9c718a6857ee7a32ed8c1a0fba2ce09f093ce
Kingmoh <rashidmincan@gmail.com>
2022-11-21 10:43:03 +1000

    Modify paths

--- data-orchestration/dags/dbt_docs_dag/scripts/generate_docs.sh
+++ data-orchestration/dags/dbt_docs_dag/scripts/generate_docs.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 
-cp -R /opt/airflow/dags/dbt /tmp;
-cd /tmp/dbt/data_community;
-/usr/local/airflow/dbt_env/bin/dbt docs generate;
+cd /opt/airflow/dags/dbt/data_community 
+/usr/local/airflow/dbt_env/bin/dbt docs generate
