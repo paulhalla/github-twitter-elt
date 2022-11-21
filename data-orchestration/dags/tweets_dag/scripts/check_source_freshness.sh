445f07fa83550091c5cea7549ef25abb1c15932e
Kingmoh <rashidmincan@gmail.com>
2022-11-21 12:39:33 +1000

    Add prod ENV vars to the push_docs_dags.py

--- data-orchestration/dags/tweets_dag/scripts/check_source_freshness.sh
+++ data-orchestration/dags/tweets_dag/scripts/check_source_freshness.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env bash
 
-if [ -d "/tmp/dbt" ]
-then
-    cd /tmp/dbt/data_community;
-    /usr/local/airflow/dbt_env/bin/dbt source freshness --profiles-dir /opt/airflow/dags/dbt/data_community --target prod
-else
-  echo "dbt project does not exist"
-  exit 1
-fi 
+
+cd /opt/airflow/dags/dbt/data_community;
+/usr/local/airflow/dbt_env/bin/dbt source freshness --profiles-dir . --target prod
 
--- data-orchestration/dags/tweets_dag/scripts/transform_tweets.sh
+++ data-orchestration/dags/tweets_dag/scripts/transform_tweets.sh
@@ -1,12 +1,5 @@
 #! /usr/bin/env bash
 
-if [ -d "/tmp/dbt" ]
-then
-  echo "dbt project already exists"
-else
-  cp -R /opt/airflow/dags/dbt /tmp
-fi 
-
-cd /tmp/dbt/data_community
+cd /opt/airflow/dags/dbt/dbt/data_community
 /usr/local/airflow/dbt_env/bin/dbt deps;
-/usr/local/airflow/dbt_env/bin/dbt build --profiles-dir /opt/airflow/dags/dbt/data_community --target prod
+/usr/local/airflow/dbt_env/bin/dbt build --profiles-dir . --target prod
